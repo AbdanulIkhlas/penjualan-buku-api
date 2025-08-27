099a830c5aed6db830162a9c1fedcd00dde50ae4
AbdanulIkhlas <muhabdanulikhlas0983@gmail.com>
2025-08-28 02:32:34 +0700

    [ADD] add views using tailwind

---
+++ app/controllers/ProductController.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class ProductController @Inject()(cc: ControllerComponents)(implicit ec: ExecutionContext)
+  extends AbstractController(cc) {
+
+  // Halaman produk
+  def products(id: Long, name: String) = Action { implicit request: Request[AnyContent] =>
+    // Di sini bisa ambil data buku atau data lain sesuai kebutuhan
+    Ok(views.html.pages.product(id, name))
+  }
+}
